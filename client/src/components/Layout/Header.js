e24a9544f8fdd511bac3b6e98a120d5ae5bd2e9a
nagaumamahesh <nandi.umamahesh.2003@gmail.com>
2024-01-28 12:01:54 +0530

    Header bug fix

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -16,31 +16,31 @@ const Header = () => {
     };
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <Link to="/" className="navbar-brand">
+                        🛒 Ecommerce App
+                    </Link>
                     <button
                         className="navbar-toggler"
                         type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarTogglerDemo01"
+                        data-toggle="collapse"
+                        data-target="#navbarTogglerDemo01"
                         aria-controls="navbarTogglerDemo01"
                         aria-expanded="false"
                         aria-label="Toggle navigation"
                     >
-                        <span className="navbar-toggler-icon" />
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <Link to="/" className="navbar-brand">
-                            🛒 Ecommerce App
-                        </Link>
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink to="/" className="nav-link ">
+                                <NavLink to="/" className="nav-link">
                                     Home
                                 </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/category" className="nav-link ">
+                                <NavLink to="/category" className="nav-link">
                                     Category
                                 </NavLink>
                             </li>
@@ -62,9 +62,9 @@ const Header = () => {
                                     <li className="nav-item dropdown">
                                         <NavLink
                                             className="nav-link dropdown-toggle"
-                                            href="#"
+                                            to="/"
                                             role="button"
-                                            data-bs-toggle="dropdown"
+                                            data-toggle="dropdown"
                                             aria-expanded="false"
                                         >
                                             {auth?.user?.name}
